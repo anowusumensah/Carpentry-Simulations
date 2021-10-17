f4948be7a3b1f1d66420361a59ceaa24dc7c2ebf
anowusumensah <92002340+anowusumensah@users.noreply.github.com>
2021-10-16 22:10:28 -0400

    Update ReturnMaxVel.m

--- ReturnMaxVel.m
+++ ReturnMaxVel.m
@@ -1,7 +1,6 @@
-
-
 function [maxVel,tVel]= ReturnMaxVel(filename)
-% This function accepts a file and return the 
+% Function written by Anthony Owusu-Mensah
+% This function accepts an action potential file and return the 
 % maximum upstroke velocity
     Data = readtable(filename);
     tdata = Data.Var1; 
@@ -10,10 +9,6 @@ function [maxVel,tVel]= ReturnMaxVel(filename)
     diffT = diff(tdata);
     gradVel = diffV./diffT;
     maxVel =  max(gradVel); % Maximum Upstroke Velocity
-    
-    tVel = tdata(find(gradVel == max (gradVel))); % Time maximum Velocity occurs
-    
-    
-    
+    tVel = tdata(find(gradVel == max (gradVel))); % Time maximum Velocity occurs 
     
 end
